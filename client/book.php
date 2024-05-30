7f11fa27b2991f32d846d544d645c99f45f28a7b
TNlap <phnt0604@gmail.com>
2024-05-30 12:48:00 +0700

    upload

--- client/book.php
+++ client/book.php
@@ -44,7 +44,7 @@
     JOIN products p ON b.product_id = p.product_id
     JOIN book_categories bc ON b.book_category_id = bc.book_category_id
     WHERE bc.book_category_name = 'Home & Garden'";
-    card_display($sqlh7, "Giải trí", $conn);
+    card_display($sqlh7, "Giải trí và Giáo dục", $conn);
 
     $sqlh8 = "SELECT TOP 2 b.book_name, p.product_image, p.product_price, p.product_id
     FROM books b
@@ -63,7 +63,7 @@
     JOIN products p ON b.product_id = p.product_id
     JOIN book_categories bc ON b.book_category_id = bc.book_category_id
     WHERE bc.book_category_name = 'Social Sciences'";
-    card_display($sqlh8, "Khoa học và xã hội", $conn);
+    card_display($sqlh8, "Khoa học và Xã hội", $conn);
 
     $sqlh9 = "SELECT TOP 2 b.book_name, p.product_image, p.product_price, p.product_id
     FROM books b
@@ -82,7 +82,7 @@
     JOIN products p ON b.product_id = p.product_id
     JOIN book_categories bc ON b.book_category_id = bc.book_category_id
     WHERE bc.book_category_name = 'Computer Science'";
-    card_display($sqlh9, "Nghệ thuật", $conn);
+    card_display($sqlh9, "Văn học và Nghệ thuật", $conn);
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
